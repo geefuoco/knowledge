bbc3aa457d52f6e8ad651d7fbba4a0c8dfbe8897
Gianluca Fuoco <gianluca_1227@hotmail.com>
2022-08-17 21:15:37 -0400

    feat: created module for better error handling

---
+++ server/src/errors/api_errors.ts
@@ -0,0 +1,103 @@
+export class APIError extends Error {
+  private _statusCode: number;
+
+  constructor(statusCode: number, message: string) {
+    super(message);
+    this._statusCode = statusCode;
+  }
+
+  get statusCode(): number {
+    return this._statusCode;
+  }
+}
+
+export enum APIErrorMessages {
+  INVALID_ID = "Invalid id. Id's should be an integer value >= 0.",
+  NOT_FOUND = "Resource was not found.",
+  SERVER_ERROR = "An internal error on the sever has occured.",
+  BAD_REQUEST = "Invalid request. Please check parameters.",
+  UNAUTHORIZED = "No credentials were provided to access this resource.",
+  FORBIDDEN = "Current user is not authorzied to access this resource.",
+  INVALID_CONTENT_LENGTH = "Invalid content length. Max length is 250 characters.",
+  INVALID_CREDENTIALS = "Invalid email or password provided",
+  INVALID_EMAIL = "Invalid email address. Please use a real email."
+}
+
+export enum StatusCodes {
+  OK = 200,
+  UNAUTHORIZED = 401,
+  FORBIDDEN = 403,
+  BAD_REQUEST = 400,
+  NOT_FOUND = 404,
+  CREATED = 201,
+  REDIRECT = 301,
+  TOO_MANY_REQUESTS = 429,
+  SERVER_ERROR = 500
+}
+
+function createApiErrors() {
+  function createInvalidIdError(): APIError {
+    return new APIError(StatusCodes.BAD_REQUEST, APIErrorMessages.INVALID_ID);
+  }
+
+  function createNotFoundError() {
+    return new APIError(StatusCodes.NOT_FOUND, APIErrorMessages.NOT_FOUND);
+  }
+
+  function createServerError(): APIError {
+    return new APIError(
+      StatusCodes.SERVER_ERROR,
+      APIErrorMessages.SERVER_ERROR
+    );
+  }
+
+  function createInvalidRequestError(): APIError {
+    return new APIError(StatusCodes.BAD_REQUEST, APIErrorMessages.BAD_REQUEST);
+  }
+
+  function createUnauthorizedError(): APIError {
+    return new APIError(
+      StatusCodes.UNAUTHORIZED,
+      APIErrorMessages.UNAUTHORIZED
+    );
+  }
+
+  function createForbiddenError(): APIError {
+    return new APIError(StatusCodes.FORBIDDEN, APIErrorMessages.FORBIDDEN);
+  }
+
+  function createInvalidEmailError(): APIError {
+    return new APIError(
+      StatusCodes.BAD_REQUEST,
+      APIErrorMessages.INVALID_EMAIL
+    );
+  }
+
+  function createInvalidContentLengthError(): APIError {
+    return new APIError(
+      StatusCodes.BAD_REQUEST,
+      APIErrorMessages.INVALID_CONTENT_LENGTH
+    );
+  }
+
+  function createInvalidCredentialsError(): APIError {
+    return new APIError(
+      StatusCodes.UNAUTHORIZED,
+      APIErrorMessages.INVALID_CREDENTIALS
+    );
+  }
+
+  return Object.freeze({
+    createInvalidIdError,
+    createNotFoundError,
+    createServerError,
+    createInvalidRequestError,
+    createUnauthorizedError,
+    createForbiddenError,
+    createInvalidEmailError,
+    createInvalidContentLengthError,
+    createInvalidCredentialsError
+  });
+}
+
+export default createApiErrors();
---
+++ server/src/errors/error_handler.ts
@@ -0,0 +1,33 @@
+import type { Request, Response, NextFunction } from "express";
+import { APIError, StatusCodes } from "./api_errors";
+
+export async function errorHandler(
+  err: Error,
+  _: Request,
+  res: Response,
+  __: NextFunction
+) {
+  res.setHeader("Content-Type", "application/json");
+  let statusCode = StatusCodes.NOT_FOUND;
+  if (err instanceof APIError) {
+    statusCode = err.statusCode;
+  }
+  res.status(statusCode).json({
+    name: err.name,
+    statusCode,
+    message: err.message || "Not found"
+  });
+}
+
+export async function notFoundHandler(
+  _: Request,
+  res: Response,
+  __: NextFunction
+) {
+  res.setHeader("Content-Type", "application/json");
+  const err = {
+    statusCode: StatusCodes.NOT_FOUND,
+    message: "Reource not found"
+  };
+  res.status(StatusCodes.NOT_FOUND).json(err);
+}
