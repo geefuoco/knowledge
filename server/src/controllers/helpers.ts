02ece2b48d4108f4e9811201ef0196169bfc19ff
Gianluca Fuoco <gianluca_1227@hotmail.com>
2022-08-17 21:16:37 -0400

    feat: added wrapper function for api routes. Created new helper functions

--- server/src/controllers/helpers.ts
+++ server/src/controllers/helpers.ts
@@ -1,4 +1,5 @@
 import type { Request, Response, NextFunction } from "express";
+import apiErrors from "../errors/api_errors";
 
 export type ExpressCallback = (
   req: Request,
@@ -6,10 +7,31 @@ export type ExpressCallback = (
   next: NextFunction
 ) => Promise<void>;
 
-export function getIdOrThrow(req: Request): number {
+export type Void = (req: Request, res: Response) => Promise<void>;
+
+export function parseIdOrThrow(req: Request): number {
   const id = Number(req.params.id);
   if (isNaN(id)) {
-    throw new Error("Invalid id");
+    throw apiErrors.createInvalidIdError();
   }
   return id;
+}
+
+export function createTimestamp(): Date {
+  return new Date(Date.now());
+}
+
+export function expressWrapper(callback: Void): ExpressCallback {
+  return async function (req: Request, res: Response, next: NextFunction) {
+    try {
+      res.setHeader("Content-Type", "application/json");
+      await callback(req, res);
+    } catch (error) {
+      next(error);
+    }
+  };
+}
+
+export function continueRoute(_: Request, __: Response, next: NextFunction) {
+  next(apiErrors.createNotFoundError());
 }
