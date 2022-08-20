2825bace16459957a6eaea286b1262dcc79d2c8a
Gianluca Fuoco <gianluca_1227@hotmail.com>
2022-08-20 16:24:54 -0400

    feat: created basic user sign up form ui

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,112 @@
+import { FormEvent, useRef, useState } from "react";
+import Form, { FormErrors } from "./components/Form";
+import TextInput from "./components/Input";
+
 function App() {
-  return <div className="2xl text-blue-200">Hello, Tailwindcss</div>;
+    const email = useRef<HTMLInputElement | null>(null);
+    const password = useRef<HTMLInputElement | null>(null);
+    const confirmPassword = useRef<HTMLInputElement | null>(null);
+    const submit = useRef<HTMLButtonElement | null>(null);
+    const [errors, setErrors] = useState<FormErrors>({});
+
+    const handleSubmit = (e: FormEvent) => {
+        e.preventDefault();
+        const emailInput = email.current;
+        const passwordInput = password.current;
+        const confirmPasswordInput = confirmPassword.current;
+        const button = submit.current;
+
+        if (!emailInput || !passwordInput || !confirmPasswordInput || !button) {
+            console.error("Error: Could not find input elements");
+            return;
+        }
+
+        button.disabled = true;
+
+        if (passwordInput.value !== confirmPasswordInput.value) {
+            console.log("Passwords dont match");
+            setErrors({
+                password: "Passwords don't match",
+                confirmPassword: "Passwords don't match",
+                ...errors,
+            });
+            return;
+        }
+        setErrors({});
+        button.disabled = false;
+    };
+    return (
+        <div className="h-full">
+            <Form
+                options={{
+                    onSubmit: handleSubmit,
+                    method: "post",
+                }}
+            >
+                <h1 className="font-bold text-3xl text-center">Sign Up</h1>
+                <br />
+                <div className="flex justify-between">
+                    <label htmlFor="email" className="text-xl font-bold">
+                        Email
+                    </label>
+                    <TextInput
+                        myref={email}
+                        type="email"
+                        required={true}
+                        name="email"
+                        placeholder="example@email.com"
+                    />
+                </div>
+                {errors && errors["email"] && (
+                    <span className="text-red-500 font-bold">
+                        {errors["email"]}
+                    </span>
+                )}
+                <div className="flex justify-between">
+                    <label htmlFor="password" className="text-xl font-bold">
+                        Password
+                    </label>
+                    <TextInput
+                        myref={password}
+                        type="password"
+                        required={true}
+                        name="password"
+                    />
+                </div>
+                {errors && errors["password"] && (
+                    <span className="text-red-500 font-bold">
+                        {errors["password"]}
+                    </span>
+                )}
+                <div className="flex justify-between">
+                    <label
+                        htmlFor="confirm-password"
+                        className="text-xl font-bold"
+                    >
+                        Confirm Password
+                    </label>
+                    <TextInput
+                        myref={confirmPassword}
+                        type="password"
+                        required={true}
+                        name="confirm-password"
+                    />
+                </div>
+                {errors && errors["confirmPassword"] && (
+                    <span className="text-red-500 font-bold">
+                        {errors["confirmPassword"]}
+                    </span>
+                )}
+                <button
+                    className="rounded-md py-4 px-8 bg-blue-400 text-xl text-white hover:bg-blue-300 transition ease-in-out duration-200"
+                    type="submit"
+                    ref={submit}
+                >
+                    Register
+                </button>
+            </Form>
+        </div>
+    );
 }
 
 export default App;
---
+++ client/src/components/Input.tsx
@@ -0,0 +1,27 @@
+export type InputOptions = {
+    type: "email" | "password" | "text";
+    required: boolean;
+    placeholder?: string;
+    name: string;
+    myref?: React.Ref<HTMLInputElement>;
+};
+const TextInput: React.FC<InputOptions> = ({
+    type,
+    required,
+    placeholder,
+    name,
+    myref,
+}: InputOptions) => {
+    return (
+        <input
+            className="p-3 leading-normal drop-shadow-md"
+            placeholder={placeholder}
+            required={required}
+            type={type}
+            name={name}
+            ref={myref}
+        />
+    );
+};
+
+export default TextInput;
