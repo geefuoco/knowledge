fe818aba2ea5f534816be927fff939b4f0352970
Gianluca Fuoco <gianluca_1227@hotmail.com>
2022-08-31 16:33:53 -0400

    fix: navbar sticks to top of page and dropdown properly behind it

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -80,123 +80,126 @@ const Navbar: React.FC = () => {
           showModal={setShowModal}
         />
       )}
-      <nav className="sticky bg-slate-400 p-4 flex text-white z-20">
-        <div className="basis-1/3 flex-grow my-auto">
-          <Link to="/">
-            <h1 className="text-3xl">Knowledge</h1>
-          </Link>
-        </div>
-        <div className="basis-1/2 flex justify-end md:hidden">
-          <button
-            className="flex flex-col gap-1 cursor-pointer"
-            onClick={() => setShowDropdown(!showDropdown)}
-            type="button"
-          >
-            <div className="py-1 px-4 bg-white rounded-sm"></div>
-            <div className="py-1 px-4 bg-white rounded-sm"></div>
-            <div className="py-1 px-4 bg-white rounded-sm"></div>
-          </button>
-        </div>
-        {user && (
-          <div className="hidden md:block basis-1/3">
-            <Search />
+      <nav className="sticky top-0  text-white">
+        <div className="relative z-10 flex w-full h-full bg-slate-400 p-4">
+          <div className="basis-1/3 flex-grow my-auto">
+            <Link to="/">
+              <h1 className="text-3xl">Knowledge</h1>
+            </Link>
           </div>
-        )}
-        <div className="basis-1/3 flex-grow justify-end hidden md:flex">
+          <div className="basis-1/2 flex justify-end md:hidden">
+            <button
+              className="flex flex-col gap-1 cursor-pointer"
+              onClick={() => setShowDropdown(!showDropdown)}
+              type="button"
+            >
+              <div className="py-1 px-4 bg-white rounded-sm"></div>
+              <div className="py-1 px-4 bg-white rounded-sm"></div>
+              <div className="py-1 px-4 bg-white rounded-sm"></div>
+            </button>
+          </div>
+          {user && (
+            <div className="hidden md:block basis-1/3">
+              <Search />
+            </div>
+          )}
+          <div className="basis-1/3 flex-grow justify-end hidden md:flex">
+            {!user ? (
+              <div className="flex gap-8">
+                <Link
+                  to="/login"
+                  className="py-1 px-3 text-md md:text-lg bg-slate-600 rounded-lg"
+                >
+                  Login
+                </Link>
+                <Link
+                  to="/register"
+                  className="py-1 px-3 text-md md:text-lg bg-slate-600 rounded-lg"
+                >
+                  Sign up
+                </Link>
+              </div>
+            ) : (
+              <div className="flex gap-2 items-center">
+                <a
+                  className="py-2 px-3 text-md bg-slate-600 rounded-lg"
+                  href="/profile"
+                >
+                  Profile
+                </a>
+                <button
+                  className="py-2 px-3 whitespace-nowrap itext-md bg-slate-600 rounded-lg"
+                  onClick={() => setShowModal(true)}
+                >
+                  New Post
+                </button>
+                <button
+                  onClick={() => {
+                    setShowModal(false);
+                    onLogout();
+                  }}
+                  className="py-2 px-3 text-md bg-slate-600 rounded-lg"
+                >
+                  Logout
+                </button>
+              </div>
+            )}
+          </div>
+        </div>
+
+        <div
+          className={`text-white transition-all duration-500 ease-in-out absolute left-0 w-full bg-slate-400 md:hidden ${
+            showDropdown ? "top-16" : "-top-96"
+          }`}
+        >
           {!user ? (
-            <div className="flex gap-8">
-              <Link
-                to="/login"
-                className="py-1 px-3 text-md md:text-lg bg-slate-600 rounded-lg"
-              >
-                Login
-              </Link>
-              <Link
-                to="/register"
-                className="py-1 px-3 text-md md:text-lg bg-slate-600 rounded-lg"
-              >
-                Sign up
-              </Link>
-            </div>
+            <>
+              <div className="text-md font-bold p-4 ">
+                <Link to="/login" onClick={() => setShowDropdown(false)}>
+                  Login
+                </Link>
+              </div>
+              <div className="text-md font-bold p-4">
+                <Link to="/register" onClick={() => setShowDropdown(false)}>
+                  Sign Up
+                </Link>
+              </div>
+            </>
           ) : (
-            <div className="flex gap-2 items-center">
-              <a
-                className="py-2 px-3 text-md bg-slate-600 rounded-lg"
-                href="/profile"
-              >
-                Profile
-              </a>
-              <button
-                className="py-2 px-3 whitespace-nowrap itext-md bg-slate-600 rounded-lg"
-                onClick={() => setShowModal(true)}
-              >
-                New Post
-              </button>
-              <button
-                onClick={() => {
-                  setShowModal(false);
-                  onLogout();
-                }}
-                className="py-2 px-3 text-md bg-slate-600 rounded-lg"
-              >
-                Logout
-              </button>
-            </div>
+            <>
+              <div className="text-md font-bold p-4">
+                <Link to="/profile" onClick={() => setShowDropdown(false)}>
+                  Profile
+                </Link>
+              </div>
+              <div className="text-md font-bold p-4">
+                <button
+                  role="button"
+                  className="block"
+                  onClick={() => {
+                    setShowModal(!showModal);
+                    setShowDropdown(false);
+                  }}
+                >
+                  New Post
+                </button>
+              </div>
+              <div className="text-md font-bold p-4">
+                <button
+                  onClick={() => {
+                    setShowDropdown(false);
+                    setShowModal(false);
+                    onLogout();
+                  }}
+                  className="block"
+                >
+                  Logout
+                </button>
+              </div>
+            </>
           )}
         </div>
       </nav>
-      <div
-        className={`text-white transition-all duration-500 ease-in-out absolute left-0 w-full bg-slate-400 md:hidden ${
-          showDropdown ? "top-16" : "-top-96"
-        }`}
-      >
-        {!user ? (
-          <>
-            <div className="text-md font-bold p-4 ">
-              <Link to="/login" onClick={() => setShowDropdown(false)}>
-                Login
-              </Link>
-            </div>
-            <div className="text-md font-bold p-4">
-              <Link to="/register" onClick={() => setShowDropdown(false)}>
-                Sign Up
-              </Link>
-            </div>
-          </>
-        ) : (
-          <>
-            <div className="text-md font-bold p-4">
-              <Link to="/profile" onClick={() => setShowDropdown(false)}>
-                Profile
-              </Link>
-            </div>
-            <div className="text-md font-bold p-4">
-              <button
-                role="button"
-                className="block"
-                onClick={() => {
-                  setShowModal(!showModal);
-                  setShowDropdown(false);
-                }}
-              >
-                New Post
-              </button>
-            </div>
-            <div className="text-md font-bold p-4">
-              <button
-                onClick={() => {
-                  setShowDropdown(false);
-                  setShowModal(false);
-                  onLogout();
-                }}
-                className="block"
-              >
-                Logout
-              </button>
-            </div>
-          </>
-        )}
-      </div>
     </>
   );
 };
